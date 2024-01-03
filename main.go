39885ae027f951e4393b1747c4270b4dd6e7c21e
ryananhtuan-nguyen <ryan.nguyen0720@gmail.com>
2024-01-03 15:39:05 +1300

    create sqlite db

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"strconv"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	database, _ := sql.Open("sqlite3", "./sample.db")
+
+	statement, _ := database.Prepare("CREATE TABLE IF NOT EXISTS people (id INTEGER PRIMARY KEY, firstname TEXT, lastname TEXT)")
+
+	statement.Exec()
+
+	statement, _ = database.Prepare("INSERT INTO people (firstname, lastname) VALUES (?,?)")
+
+	statement.Exec("John", "Doe")
+
+	rows, _ := database.Query("SELECT id, firstname, lastname FROM people")
+
+	var id int
+	var firstname, lastname string
+
+	for rows.Next() {
+		rows.Scan(&id, &firstname, &lastname)
+		fmt.Println(strconv.Itoa(id) + ":" + firstname + " " + lastname)
+	}
+
+}
